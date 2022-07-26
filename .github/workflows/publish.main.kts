7c6fe60179d1fa68e9f020426d6c3946f2fd9088
andylamax <andylamax@programmer.net>
2022-07-27 00:43:11 +0300

    Corrected publish scripts

--- .github/workflows/publish.main.kts
+++ .github/workflows/publish.main.kts
@@ -7,19 +7,22 @@ import it.krzeminski.githubactions.actions.actions.CheckoutV3
 import it.krzeminski.githubactions.actions.actions.SetupJavaV3
 import it.krzeminski.githubactions.actions.actions.SetupJavaV3.Distribution.Corretto
 import it.krzeminski.githubactions.actions.gradle.GradleBuildActionV2
+import it.krzeminski.githubactions.domain.Job
 import it.krzeminski.githubactions.domain.RunnerType.MacOSLatest
 import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
 import it.krzeminski.githubactions.domain.triggers.Push
 import it.krzeminski.githubactions.dsl.StringCustomValue
 import it.krzeminski.githubactions.dsl.WorkflowBuilder
+import it.krzeminski.githubactions.dsl.expressions.expr
 import it.krzeminski.githubactions.dsl.workflow
 import it.krzeminski.githubactions.yaml.toYaml
 
 fun WorkflowBuilder.submoduleJob(
     prefix: String,
     project: String,
-    task: String
-) = job(id = "$prefix-$project", runsOn = MacOSLatest) {
+    task: String,
+    needs: List<Job> = listOf()
+) = job(id = "$prefix-$project", runsOn = MacOSLatest, needs = needs) {
     uses(CheckoutV3(submodules = true))
     uses(SetupJavaV3("18", Corretto))
     run(
@@ -31,19 +34,27 @@ fun WorkflowBuilder.submoduleJob(
 
 fun WorkflowBuilder.buildJob(project: String) = submoduleJob("build", project, "build")
 
-fun WorkflowBuilder.publishJob(project: String) = submoduleJob("publish", project, "publishToSonatype closeAndReleaseStagingRepository")
+fun WorkflowBuilder.publishJob(project: String, needs: Job) = submoduleJob(
+    "publish", project, "publishToSonatype closeAndReleaseStagingRepository", needs = listOf(needs)
+)
 
 val workflow = workflow(
     name = "Build And Publish",
     on = listOf(Push(branches = listOf("main"))),
     sourceFile = __FILE__.toPath(),
+    env = linkedMapOf(
+        "ASOFT_MAVEN_PGP_PRIVATE_KEY" to expr { secrets["ASOFT_MAVEN_PGP_PRIVATE_KEY"].toString() },
+        "ASOFT_MAVEN_PGP_PASSWORD" to expr { secrets["ASOFT_MAVEN_PGP_PASSWORD"].toString() },
+        "ASOFT_NEXUS_PASSWORD" to expr { secrets["ASOFT_NEXUS_PASSWORD"].toString() },
+        "ASOFT_NEXUS_USERNAME" to expr { secrets["ASOFT_NEXUS_USERNAME"].toString() },
+    )
 ) {
     val projects = listOf("live", "viewmodel")
     val buildJobs = projects.map { project -> buildJob(project) }
-    job(id = "rendezvous", runsOn = UbuntuLatest, needs = buildJobs) {
+    val rendezvous = job(id = "rendezvous", runsOn = UbuntuLatest, needs = buildJobs) {
         run("""echo "all builds completed. Beginning deployment"""")
     }
-    projects.map { project -> publishJob(project) }
+    projects.map { project -> publishJob(project, rendezvous) }
 }
 
 println(workflow.toYaml(addConsistencyCheck = false))
